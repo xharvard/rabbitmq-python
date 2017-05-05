9ede12697c27a39b5fdb717dc8b40d8bd090b62c
xharvard <xhhycit@163.com>
2017-05-05 14:42:55 +0800

    add python scripts

---
+++ scripts-python/receive_logs_direct.py
@@ -0,0 +1,31 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs', type='direct')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+serverities = sys.argv[1:]
+if not serverities:
+	print >> sys.stderr, "Usage: %s [info] [warning] [error]" % (sys.argv[0],)
+	sys.exit(1)
+
+for serverity in serverities:
+	channel.queue_bind(exchange='direct_logs',
+					queue=queue_name,
+					routing_key=serverity)
+
+print ' [*] Waiting for logs. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+	print " [x] %r:%r" % (method.routing_key,body,)
+
+channel.basic_consume(callback,
+					  queue=queue_name,
+					  no_ack=True)
+
+channel.start_consuming()
